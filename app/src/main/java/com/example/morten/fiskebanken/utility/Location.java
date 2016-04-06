d6085a0bd247b4a5c510ff8bc16c4439c0e93e31
Mortenher <morten@hermanseter.com>
2016-04-06 16:59:05 +0200

    Fiskebanken map-fixing

---
+++ app/src/main/java/com/example/morten/fiskebanken/utility/Location.java
@@ -0,0 +1,32 @@
+package com.example.morten.fiskebanken.utility;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Morten on 06.04.2016.
+ */
+public class Location implements Parcelable {
+
+    Location Loc;
+
+    public Location(Location loc){
+        Loc = loc;
+    }
+
+    public Location(Parcel in){
+        String[] data = new String[1];
+        in.readStringArray(data);
+        this.Loc = data[0];
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+}
